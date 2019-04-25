880964e60c448c9184e44d5b7a0fbd414e8f703a
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-04-25 17:40:12 +0300

    Fix config

--- src/test/java/com/rbkmoney/wb/list/manager/WbListManagerApplicationTest.java
+++ src/test/java/com/rbkmoney/wb/list/manager/WbListManagerApplicationTest.java
@@ -83,7 +83,7 @@ public class WbListManagerApplicationTest extends KafkaAbstractTest {
             .withPrivilegedMode(true)
             .waitingFor(new HttpWaitStrategy()
                     .forStatusCode(200)
-                    .forPort(8087)
+                    .forPort(8098)
                     .forPath("/ping"))
             .withStartupTimeout(Duration.ofMinutes(5));
 
--- src/test/java/com/rbkmoney/wb/list/manager/WbListSafetyApplicationTest.java
+++ src/test/java/com/rbkmoney/wb/list/manager/WbListSafetyApplicationTest.java
@@ -4,6 +4,7 @@ import com.rbkmoney.damsel.wb_list.ChangeCommand;
 import com.rbkmoney.damsel.wb_list.Command;
 import com.rbkmoney.damsel.wb_list.Event;
 import com.rbkmoney.damsel.wb_list.ListType;
+import com.rbkmoney.serializer.ThriftSerializer;
 import com.rbkmoney.wb.list.manager.exception.RiakExecutionException;
 import com.rbkmoney.wb.list.manager.repository.ListRepository;
 import com.rbkmoney.wb.list.manager.serializer.EventDeserializer;
@@ -12,20 +13,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.KafkaContainer;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -37,8 +49,9 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestPropertySource(properties = {"retry.timeout=100"})
-@ContextConfiguration(classes = WbListManagerApplication.class)
-public class WbListSafetyApplicationTest extends KafkaAbstractTest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@ContextConfiguration(classes = WbListManagerApplication.class, initializers = WbListSafetyApplicationTest.Initializer.class)
+public class WbListSafetyApplicationTest {
 
     private static final String VALUE = "value";
     private static final String SHOP_ID = "shopId";
@@ -56,6 +69,44 @@ public class WbListSafetyApplicationTest extends KafkaAbstractTest {
 
     @Value("${kafka.wblist.topic.event.sink}")
     public String topicEventSink;
+
+    public static final String KAFKA_DOCKER_VERSION = "5.0.1";
+
+    @ClassRule
+    public static KafkaContainer kafka = new KafkaContainer(KAFKA_DOCKER_VERSION).withEmbeddedZookeeper();
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues
+                    .of("kafka.bootstrap.servers=" + kafka.getBootstrapServers())
+                    .applyTo(configurableApplicationContext.getEnvironment());
+            initTopic("wb-list-command");
+            initTopic("wb-list-event-sink");
+        }
+
+        @NotNull
+        private <T> Consumer<String, T> initTopic(String topicName) {
+            Consumer<String, T> consumer = createConsumer();
+            try {
+                consumer.subscribe(Collections.singletonList(topicName));
+                consumer.poll(Duration.ofSeconds(1));
+            } catch (Exception e) {
+                log.error("KafkaAbstractTest initialize e: ", e);
+            }
+            consumer.close();
+            return consumer;
+        }
+    }
+
+    public static <T> Producer<String, T> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "CLIENT");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ThriftSerializer.class);
+        return new KafkaProducer<>(props);
+    }
 
     @Test
     public void kafkaRowTestException() throws Exception {
@@ -95,14 +146,14 @@ public class WbListSafetyApplicationTest extends KafkaAbstractTest {
         return row;
     }
 
-    public static Consumer<String, Event> createConsumer() {
+
+    public static <T> Consumer<String, T> createConsumer() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "CLIENT");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, EventDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         return new KafkaConsumer<>(props);
     }
 
