4039c85e0bcc426ad8f1cb48948ba860d256b42e
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-07-03 11:23:43 +0300

    Fix path

--- src/main/java/com/rbkmoney/wb/list/manager/resource/FraudInspectorServlet.java
+++ src/main/java/com/rbkmoney/wb/list/manager/resource/FraudInspectorServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet("/v1/wb_list")
+@WebServlet("/")
 @RequiredArgsConstructor
 public class FraudInspectorServlet extends GenericServlet {
 
