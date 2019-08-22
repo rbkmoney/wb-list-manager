acc980e6a071afa9441ae6f8e14bfff36e621b20
Kostya <kennny007@yandex.ru>
2019-08-22 15:03:45 +0300

    Add implementation for new method (#15)

--- src/main/java/com/rbkmoney/wb/list/manager/converter/CommandToRowConverter.java
+++ src/main/java/com/rbkmoney/wb/list/manager/converter/CommandToRowConverter.java
@@ -1,14 +1,24 @@
 package com.rbkmoney.wb.list.manager.converter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rbkmoney.damsel.wb_list.ChangeCommand;
+import com.rbkmoney.damsel.wb_list.CountInfo;
+import com.rbkmoney.wb.list.manager.model.CountInfoModel;
 import com.rbkmoney.wb.list.manager.model.Row;
 import com.rbkmoney.wb.list.manager.utils.KeyGenerator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 
 @Component
+@RequiredArgsConstructor
 public class CommandToRowConverter implements Converter<ChangeCommand, Row> {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public Row convert(ChangeCommand command) {
@@ -16,7 +26,23 @@ public class CommandToRowConverter implements Converter<ChangeCommand, Row> {
         com.rbkmoney.damsel.wb_list.Row commandRow = command.getRow();
         String key = KeyGenerator.generateKey(commandRow);
         row.setKey(key);
-        row.setValue(commandRow.getValue());
+        row.setValue(initValue(commandRow));
         return row;
+    }
+
+    private String initValue(com.rbkmoney.damsel.wb_list.Row commandRow) {
+        if (commandRow.isSetRowInfo() && commandRow.getRowInfo().isSetCountInfo()) {
+            try {
+                CountInfo countInfo = commandRow.getRowInfo().getCountInfo();
+                String startCountTime = countInfo.isSetStartCountTime() ? countInfo.getStartCountTime() : Instant.now().toString();
+                CountInfoModel countInfoModel = new CountInfoModel(countInfo.getCount(),
+                        countInfo.getTimeToLive(),
+                        startCountTime);
+                return objectMapper.writeValueAsString(countInfoModel);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return commandRow.getValue();
     }
 }
