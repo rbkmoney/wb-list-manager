af2b7c3c7ee59418c409a512c7c4d213f3e33ef2
ggmaleva <griwania@gmail.com>
2021-09-30 11:16:58 +0300

    modify riak test

--- src/test/java/com/rbkmoney/wb/list/manager/RiakTest.java
+++ src/test/java/com/rbkmoney/wb/list/manager/RiakTest.java
@@ -5,7 +5,6 @@ import com.basho.riak.client.api.commands.kv.FetchValue;
 import com.basho.riak.client.core.query.Location;
 import com.basho.riak.client.core.query.Namespace;
 import com.basho.riak.client.core.query.RiakObject;
-import com.rbkmoney.wb.list.manager.config.RiakConfig;
 import com.rbkmoney.wb.list.manager.extension.RiakContainerExtension;
 import com.rbkmoney.wb.list.manager.model.Row;
 import com.rbkmoney.wb.list.manager.repository.ListRepository;
@@ -13,8 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -23,9 +21,8 @@ import java.util.concurrent.ExecutionException;
 import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 @ExtendWith({SpringExtension.class, RiakContainerExtension.class})
-@ContextConfiguration(classes = {ListRepository.class, RiakConfig.class})
-@TestPropertySource(properties = {"riak.bucket=wblist", "riak.address=localhost"})
 public class RiakTest {
 
     private static final String VALUE = "value";
